5c65ae974775a7cdac65a68b6b65835cc39adf20
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-11-14 15:31:54 +0100

    Add project files.

---
+++ 3,3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Uppgift_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int timpris = 80;
+            int maxDagskostnad = 950;
+
+           
+            Console.Write("Hur många timmar vill du hyra bilen? ");
+            int timmar = int.Parse(Console.ReadLine());
+
+           
+            int kostnad = timmar * timpris;
+
+           
+            if (kostnad > maxDagskostnad)
+            {
+                kostnad = maxDagskostnad;
+            }
+
+           
+            Console.WriteLine("Det kommer att kosta: " + kostnad + " kr");
+        }
+    }
+}
